3247adeec2fc90f3da80738fd00e0fd51f26e73a
Lylik <lylik1970@mail.ru>
2022-10-16 12:46:38 +0300

    task002 min max

---
+++ Task002/Program.cs
@@ -0,0 +1,18 @@
+﻿//  2. Даны два числа. Показать большее и меньшее число
+int a = 4;
+int b = 1;
+int max = a;
+int min = a;
+
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("Максимальное их 2-х чисел = " + max);
+Console.WriteLine("Минимальное их 2-х чисел = " + min);
