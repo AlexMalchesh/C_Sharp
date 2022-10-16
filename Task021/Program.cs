e31cf95ebd73cba0d9d4659ee404aa392a925a55
Lylik <lylik1970@mail.ru>
2022-10-16 13:04:46 +0300

    task021 полиндром

---
+++ Task021/Program.cs
@@ -0,0 +1,31 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+
+Console.WriteLine("Программа проверяет пятизначное число на палиндромом");
+
+int GetNum()
+{
+    Console.Write("Введите целое пятизначное число: ");
+    //return Convert.ToInt32(Console.ReadLine());
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = GetNum();
+int temp = a;
+int b = 0;
+int n = 5;
+int[] arr = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    arr[i] = temp % 10;
+    temp = temp / 10;
+}
+
+for (int i = 0; i < n; i++)
+{
+    b = b * 10 + arr[i];
+}
+
+if (a == b)
+    Console.WriteLine($"Число {a} - полиндром");
+else Console.WriteLine($"Число {a} НЕ полиндром");
