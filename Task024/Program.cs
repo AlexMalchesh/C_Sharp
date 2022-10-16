1ba00b362f54b8c2249ecd75e0a6a53a2d55f6bf
Lylik <lylik1970@mail.ru>
2022-10-16 13:08:01 +0300

    task24 кубы чисел от 1 до N

---
+++ Task024/Program.cs
@@ -0,0 +1,18 @@
+﻿// 24. Найти кубы чисел от 1 до N
+
+int GetN()
+{
+    Console.Write("Введите целое число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Cube(int n)
+{
+    return n * n * n;
+}
+
+int n = GetN();
+
+for (int i=1;i<=n;i++){
+    Console.WriteLine($"{i}^3 = {Cube(i)}");
+}
