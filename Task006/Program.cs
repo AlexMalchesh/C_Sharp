3cbd063e179b546237fc3a2fce452211a1bfdf64
Lylik <lylik1970@mail.ru>
2022-10-16 12:55:08 +0300

    task006 проверка четного числа

---
+++ Task006/Program.cs
@@ -0,0 +1,17 @@
+﻿//  6. Выяснить является ли число чётным
+
+int GetValue()
+{
+    Console.WriteLine("Введите числа: ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int value = GetValue();
+
+if (value % 2 == 0)
+{
+    Console.WriteLine("Число " + value + " является четным");
+} else {
+    Console.WriteLine("Число " + value + " НЕ является четным");
+}
