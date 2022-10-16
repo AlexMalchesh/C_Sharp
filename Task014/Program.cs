48c8ad2632d8a79656345032d208afcc60986be7
Lylik <lylik1970@mail.ru>
2022-10-16 13:01:30 +0300

    task014 3-ецифра числа

---
+++ Task014/Program.cs
@@ -0,0 +1,32 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+
+// char GetTherdNumLeft(int value)
+// {
+//     return value.ToString()[2];
+// }
+
+int GetValue()
+{
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetTherdNum(int value)
+{
+    return (value / 100) % 10;
+}
+
+int value = GetValue();
+
+if (value < 0)
+{
+    value = Math.Abs(value);
+}
+if (value > 99)
+{
+    Console.WriteLine($"Третья цифра числа = {GetTherdNum(value)}");
+}
+else
+{
+    Console.WriteLine($"Третья цифра числа отсутствует");
+}
