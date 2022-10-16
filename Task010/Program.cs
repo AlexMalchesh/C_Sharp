09b6608686ae2f246d37d5ed5120b52d02434651
Lylik <lylik1970@mail.ru>
2022-10-16 12:58:36 +0300

    task010 вторая цифра 3х значного числа

---
+++ Task010/Program.cs
@@ -0,0 +1,27 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+// int value = 475;
+
+// int result = (value / 10) % 10;
+
+// Console.WriteLine(result);
+
+int GetValue()
+{
+    Console.WriteLine("Введите трехзначное число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSecondNum(int value)
+{
+    return (value / 10) % 10;
+}
+
+void PrintResult(int res)
+{
+    Console.WriteLine("Вторая цифра числа = " + res);
+}
+
+int value = GetValue();
+int result = GetSecondNum(value);
+PrintResult(result);
