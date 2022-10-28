71460ea3b82a44ab7cdd6ef7a4bba11e011f2cd2
Lylik <lylik1970@mail.ru>
2022-10-28 20:49:43 +0300

    task36 количество четных и нечетных чисел в массиве

--- Task031/Program.cs
+++ Task031/Program.cs
@@ -12,6 +12,22 @@ int[] FillArray(int n)
     return array;
 }
 
+// for (int i = 0; i < n; i++)
+// {
+//     if (i == 0)
+//     {
+//         Console.Write($"[{array[i]} ,");
+//     }
+//     else if (i == n - 1)
+//     {
+//         Console.Write($"{array[i]}]");
+//     }
+//     else
+//     {
+//         Console.Write($"{array[i]} ,");
+//     }
+// }
+
 int[] array = FillArray(8);
 
 for (int i = 0; i < array.Length; i++)
---
+++ Task036/Program.cs
@@ -0,0 +1,41 @@
+﻿// 36. Задать массив, заполнить случайными положительными 
+// трёхзначными числами. 
+// Показать количество нечетных\четных чисел
+
+int[] FillArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+int[] CountNum(int[] array)
+{
+    int[] nums = { 0, 0 };
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            nums[0]++;
+        else
+            nums[1]++;
+    }
+
+    return nums;
+}
+
+int[] ar = FillArray(11);
+
+for (int i = 0; i < ar.Length; i++)
+{
+    Console.Write($"{ar[i]} ");
+}
+
+Console.WriteLine();
+
+int[] nums = CountNum(ar);
+
+Console.WriteLine($"Число четных чисел в массиве = {nums[0]}");
+Console.WriteLine($"Число НЕчетных чисел в массиве = {nums[1]}");
