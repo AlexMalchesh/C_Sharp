0171b25bb4ccde1822021fc0d75a15b61458793f
Lylik <lylik1970@mail.ru>
2022-10-28 20:40:12 +0300

    task27 сумма цифр в числе

---
+++ Task027/Program.cs
@@ -0,0 +1,25 @@
+﻿// 27. Определить количество цифр в числе
+
+int GetNum()
+{
+    Console.Write("Введите целое число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// double GetNum2()
+// {
+//     Console.Write("Введите целое число: ");
+//     return Convert.ToDouble(Console.ReadLine());
+// }
+
+int num = Math.Abs(GetNum());
+
+int count = 0;
+while (num != 0)
+{
+   
+    num = num /10; 
+    ++count;
+}
+
+Console.WriteLine($"количество цифр в числе = {count}");
